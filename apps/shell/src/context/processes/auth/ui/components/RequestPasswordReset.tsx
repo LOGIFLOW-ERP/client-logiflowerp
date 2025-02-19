dccfaedaef3bed495b02fff8fb44ffc94754ac20
usuario0888 <antonio.mejia@logiflowerp.com>
2025-02-18 19:01:08 -0500

    implement RequestPasswordReset component with email input and navigation link

--- apps/shell/src/context/processes/auth/ui/components/RequestPasswordReset.tsx
+++ apps/shell/src/context/processes/auth/ui/components/RequestPasswordReset.tsx
@@ -1,5 +1,48 @@
+import { Box, Button, Divider, Link, TextField } from '@mui/material';
+import { useStore } from '@shared/ui/hooks';
+import { useNavigate } from 'react-router-dom';
+
 export function RequestPasswordReset() {
-  return (
-    <div>RequestPasswordReset</div>
-  )
+
+	const { actions: { setState }, state } = useStore('auth')
+	const navigate = useNavigate()
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setState({ [e.target.name]: e.target.value })
+	}
+
+	return (
+		<form>
+			<TextField
+				label='Correo electrónico'
+				variant='outlined'
+				fullWidth
+				margin='dense'
+				size='small'
+				onChange={handleChange}
+				name='email'
+				value={state.email}
+				autoFocus
+			/>
+			<Button
+				variant='contained'
+				color='primary'
+				fullWidth
+				sx={{ marginTop: 2 }}
+			>
+				Enviar Solicitud
+			</Button>
+			<Divider sx={{ marginTop: 2, marginBottom: 2 }} />
+			<Box sx={{ textAlign: 'center' }}>
+				<Link
+					variant='body2'
+					color='primary'
+					sx={{ display: 'block', marginBottom: 1, cursor: 'pointer' }}
+					onClick={() => navigate('/sign-in')}
+				>
+					Iniciar sesión
+				</Link>
+			</Box>
+		</form>
+	)
 }
