5729a4f4b4d20826a9316cf758fc6371b329c1ae
usuario0888 <antonio.mejia@logiflowerp.com>
2025-02-20 20:52:49 -0500

    feat(env): add TypeScript definitions for environment variables in Vite

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_BASE_URL_CONFIGURATION: string
+    readonly VITE_API_BASE_URL_LOGISTICS: string
+    // más variables de entorno...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
