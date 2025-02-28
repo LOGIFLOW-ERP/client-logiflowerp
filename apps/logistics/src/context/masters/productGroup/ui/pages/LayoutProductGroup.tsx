18244bf17e5a73798c510c422c3f305831acb60c
usuario0888 <antonio.mejia@logiflowerp.com>
2025-02-28 07:44:28 -0500

    feat(RouterProvider): add LayoutProductGroup route and export as default

--- apps/logistics/src/context/masters/productGroup/ui/pages/LayoutProductGroup.tsx
+++ apps/logistics/src/context/masters/productGroup/ui/pages/LayoutProductGroup.tsx
@@ -1,4 +1,4 @@
-export function LayoutProductGroup() {
+export default function LayoutProductGroup() {
   return (
     <div>LayoutProductGroup</div>
   )
--- apps/shell/src/app/ui/providers/RouterProvider/logistics.tsx
+++ apps/shell/src/app/ui/providers/RouterProvider/logistics.tsx
@@ -3,6 +3,7 @@ import { lazy } from 'react'
 import { RouteObject } from 'react-router-dom'
 
 const LayoutMovement = lazy(() => import('../../../../../../logistics/src/context/masters/movement/ui/pages/LayoutMovement'))
+const LayoutProductGroup = lazy(() => import('../../../../../../logistics/src/context/masters/productGroup/ui/pages/LayoutProductGroup'))
 
 export const childrenLogistics: RouteObject[] = [
     {
@@ -13,6 +14,10 @@ export const childrenLogistics: RouteObject[] = [
             {
                 path: 'movement',
                 Component: LayoutMovement,
+            },
+            {
+                path: 'product Group',
+                Component: LayoutProductGroup,
             }
         ]
     },
